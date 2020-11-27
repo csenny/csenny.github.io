bc7c41a6b0f3ec7e83dca3b8554bbe264f87b155
Jon <jonathan-kings@hotmail.com>
2020-11-27 17:59:16 +0100

    Implement zooming onto project images and navigate with arrow keys

---
+++ project.js
@@ -0,0 +1,47 @@
+
+function clamp(t, a, b){
+    return t < a ? a : t > b ? b : t;
+}
+
+window.onload = function(){
+    let images = document.querySelectorAll("img[x-zoom]");
+    let imageDisplay = document.querySelector("#image-display");
+    let next = document.querySelector("#next");
+    let prev = document.querySelector("#prev");
+    let displayed = document.querySelector("#displayed");
+    let selectedIndex;
+    let moveToImage = function (index) {
+        index = clamp(index, 0, images.length - 1);
+        if(index == selectedIndex) return;
+        selectedIndex = index;
+        next.setAttribute('valid', '' + selectedIndex < images.length - 1);
+        prev.setAttribute('valid', '' + selectedIndex > 0);
+        displayed.src = images[selectedIndex].src;
+    };
+    imageDisplay.addEventListener('click', function () {
+        imageDisplay.setAttribute('hidden', 'true');
+    });
+    document.addEventListener('keydown', function (e) {
+        if(!imageDisplay.hasAttribute('hidden')){
+            if(e.key == 'ArrowLeft' || e.keyCode == 37){
+                moveToImage(selectedIndex-1);
+            }else if(e.key == 'ArrowRight' || e.keyCode == 39){
+                moveToImage(selectedIndex+1);
+            }
+        }
+    });
+    next.addEventListener('click', function (e) {
+        moveToImage(selectedIndex+1);
+        e.stopPropagation();
+    });
+    prev.addEventListener('click', function (e) {
+        moveToImage(selectedIndex-1);
+        e.stopPropagation();
+    });
+    images.forEach(function (image, index) {
+        image.addEventListener('click', function () {
+            imageDisplay.removeAttribute('hidden');
+            moveToImage(index);
+        });
+    })
+};
