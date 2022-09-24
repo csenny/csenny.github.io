1416f645b309d0c364c70b00810a5c936c2e46bd
Jon <jonathan-kings@hotmail.com>
2022-09-24 11:45:38 +0200

    Re-enable small image resizing

--- image-sizes.bat
+++ image-sizes.bat
@@ -1,21 +1,21 @@
 @echo off
 
-rem Make a copy of all png/jpg images in all subdirectories to make them small and fast to load, & medium/appropriately sized
+rem Makes a copy of all png/jpg images in all subdirectories to make them small and fast to load, & medium/appropriately sized
 
 for /d %%i in (./*) do (
     cd "%%i"
 
-    @REM rm -rf small
-    @REM mkdir small
-    @REM cp *.png small/
-    @REM cp *.jpeg small/
-    @REM cp *.jpg small/
-    @REM cd small && chmod -R 777 .
-    @REM magick mogrify -resize 128x128 -blur 8 *.png
-    @REM magick mogrify -resize 128x128 -blur 8 *.jpeg
-    @REM magick mogrify -resize 128x128 -blur 8 *.jpg
-    @REM cd ..
-    @REM rd /q small
+    rm -rf small
+    mkdir small
+    cp *.png small/
+    cp *.jpeg small/
+    cp *.jpg small/
+    cd small && chmod -R 777 .
+    magick mogrify -resize 128x128 -blur 8 *.png
+    magick mogrify -resize 128x128 -blur 8 *.jpeg
+    magick mogrify -resize 128x128 -blur 8 *.jpg
+    cd ..
+    rd /q small
     
     rm -rf medium
     mkdir medium
